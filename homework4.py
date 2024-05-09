b9a01aa239675465035d7911c3496dfc69f79226
M0RECH0K <168891004+M0RECH0K@users.noreply.github.com>
2024-05-09 10:37:32 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+immutable_var = 1, 'apple', 2, 'banana', True
+print(immutable_var)
+#immutable_var[0] = 5
+#print(immutable_var)
+mutable_list = [1, 'Kharlamov', 2, 'Dmitry']
+print(mutable_list)
+mutable_list.append(3)
+print(mutable_list)
+mutable_list.extend(['Borisovich'])
+print(mutable_list)
